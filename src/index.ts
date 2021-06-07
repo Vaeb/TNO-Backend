e19a67fdca42426ff124e5d8a850edddedfe87b7
Adam Johnson <vaeb@hey.com>
2021-06-07 01:03:27 +0100

    Https

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
 import 'dotenv/config';
-
+import fs from 'fs';
 import express from 'express';
 import cors from 'cors';
+import https from 'https';
 
 import { log } from './utils';
 import * as initialData from './data/initialData';
+
+const domain = 'vaeb.io';
+const key = fs.readFileSync(`/etc/letsencrypt/live/${domain}/privkey.pem`, 'utf8');
+const cert = fs.readFileSync(`/etc/letsencrypt/live/${domain}/cert.pem`, 'utf8');
+const chain = fs.readFileSync(`/etc/letsencrypt/live/${domain}/cert.pe`, 'utf8');
+const httpsOptions = {
+    key,
+    cert,
+    ca: chain,
+};
 
 const app = express();
 
@@ -29,6 +40,12 @@ app.get('/streams/:faction', (req, res) => {
 
 app.use((_req, res) => res.send({ error: 'This is not a valid API endpoint.' }));
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
+// app.listen(process.env.PORT, () => {
+//     console.log(`Server running on port ${process.env.PORT}`);
+// });
+
+const server = https.createServer(httpsOptions, app);
+
+server.listen(process.env.PORT, () => {
+    console.log(`server starting on port : ${process.env.PORT}`);
 });
