78c32bc39c24d19811a05566f0c60aac9acad706
Adam Johnson <vaeb@hey.com>
2021-06-18 23:42:26 +0100

    character updates: den

--- src/data/characters.ts
+++ src/data/characters.ts
@@ -463,6 +463,7 @@ export const npCharacters: NpCharacters = {
     ],
     Deansocool: [
         { name: 'Dean Quincy', faction: 'NBC', displayName: 0, assume: 'assumeNpNoOther' },
+        { name: '[Ride Along] Den Shiesty', faction: 'Police' },
     ],
     Denior13: [
         { name: 'Keith "Bulldog" Dooger', faction: 'Lost MC' },
