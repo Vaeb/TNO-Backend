70fddf53e758fbf962046a860c296d5957a65b53
Adam Johnson <vaeb@hey.com>
2021-06-19 16:19:11 +0100

    character updates: np

--- src/data/settings.ts
+++ src/data/settings.ts
@@ -8,7 +8,7 @@ export const stopOnMin = true;
 export const checkOther = true;
 export const intervalSeconds = 0.7;
 
-export const regNp = /(?<!not\s)(?:no[\s\-_.]*pixel|\bn[\s\-_.]*p\b)(?![\s\-]+inspired)/i;
+export const regNp = /(?<!not\s)(?:no[\s\-_.]*pixel|\bn[\s\-_.]*p\b)(?![\s\-]*(?:inspired|ban))/i;
 export const regNpPublic = mergeRegex([
     /(?:no[\s\-_.]*pixel|\bn[\s\-_.]*p(?=\b|\d))(?:[\s\-_.]*(?:rp|\d+\.?\d*))?[\W_]*pub\w*\b(?!\W+(?:later|after))/i,
     /|(\bpub\w*[\W_]*(?:no[\s\-_.]*pixel|\bn[\s\-_.]*p(?=\b|\d)))/i,
