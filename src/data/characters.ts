f62198793416a84a5d826f339222d6b7094b907e
Adam Johnson <vaeb@hey.com>
2021-06-21 02:16:33 +0100

    character updates: carmella

--- src/data/characters.ts
+++ src/data/characters.ts
@@ -326,7 +326,7 @@ export const npCharacters: NpCharacters = {
         { name: 'Dimitri Nekola', factions: ['Pegasus'] },
     ],
     Carmen: [
-        { name: 'Carmella Corset', factions: ['RR'] },
+        { name: 'Carmella Corset', factions: ['Rooster'] },
     ],
     Carter: [
         { name: 'Spencer Smith', nicknames: ['Mormon'] },
