b1ad43ba7013da71bf0ad6396ca847c5a919993c
Adam Johnson <vaeb@hey.com>
2021-08-04 02:15:57 +0100

    character updates: briddle

--- src/data/characters.ts
+++ src/data/characters.ts
@@ -2247,8 +2247,8 @@ export const npCharacters: NpCharacters = {
         { name: 'Jack Valentino', factions: ['Lunatix'] },
     ],
     UberHaxorNova: [
-        { name: '[Deputy] Barry Briddle', factions: ['Police'], nicknames: [reg(/\bbriddl/)] },
         { name: 'Siz Fulker', factions: ['HOA'], leader: true, nicknames: ['uhnSuffer', 'mechanic', reg(/\bloans?\b/), reg(/\bsiz/)] },
+        { name: '[Deputy] Barry Briddle', factions: ['Police'], nicknames: [reg(/\bbriddl/)] },
         { name: 'Toh Biggles Fitzcharles', displayName: 0, nicknames: ['Yaes'] },
     ],
     uhLuvi: [
