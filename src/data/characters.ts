b05a613c72e138642ac06274479b642a5a9b518e
Adam Johnson <vaeb@hey.com>
2021-08-14 01:33:26 +0100

    character updates: max

--- src/data/characters.ts
+++ src/data/characters.ts
@@ -1152,7 +1152,7 @@ export const npCharacters: NpCharacters = {
     ],
     KinkyHobo_: [
         { name: '[Lawyer //] Gill Schultz', factions: ['DoJ'], displayName: 1 },
-        { name: '[Deputy] Max Muller', factions: ['Police'], displayName: 2 },
+        { name: '[Officer] Max Muller', factions: ['Police'], displayName: 2 },
     ],
     Kitboga: [
         { name: 'Edna Moose' },
