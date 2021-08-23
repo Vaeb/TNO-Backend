2b70b57ef65eb7cbeca69309c771181880b2d62d
Adam Johnson <vaeb@hey.com>
2021-08-23 17:50:58 +0100

    character updates: reynolds

--- src/data/characters.ts
+++ src/data/characters.ts
@@ -738,7 +738,7 @@ export const npCharacters: NpCharacters = {
         { name: 'Richard Chiclets', factions: ['Burger Shot'], nicknames: ['Dick Chiclets'], displayName: 3 },
     ],
     Flynno: [
-        { name: '[Officer] ? Reynolds', factions: ['Police'], displayName: 3, assume: 'assumeNpNoOther' },
+        { name: '[Officer] Mickey Reynolds', factions: ['Police'], displayName: 3, assume: 'assumeNpNoOther' },
     ],
     Forlorn79: [
         { name: '[EMS] Amoris Pax', factions: ['Medical'] },
