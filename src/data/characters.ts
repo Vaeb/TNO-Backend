7f7a741391fa521282a68dc121e8fa1fa8440701
Adam Johnson <vaeb@hey.com>
2021-08-29 20:28:25 +0100

    character updates: cop

--- src/data/characters.ts
+++ src/data/characters.ts
@@ -1277,6 +1277,7 @@ export const npCharacters: NpCharacters = {
         { name: 'Alabaster Slim', nicknames: ['Pimp'], displayName: 2 },
         { name: 'Kegal Dan', factions: ['Dans'] },
         { name: 'Brett Biggledoinks' },
+        { name: 'Cop Killa', displayName: 0 },
         { name: 'Sigfried Porsche', nicknames: ['porshe'], displayName: 0 },
         { name: 'Hampton Brandon', nicknames: ['TTD', 'Ten Toes Down'] },
         { name: 'Tame "The Mandalorian" Saxon', nicknames: ['Mandolorian'] },
