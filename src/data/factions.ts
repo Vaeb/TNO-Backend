1ea01f708f6ef69e01081e0089bc6c59e4471672
Adam Johnson <vaeb@hey.com>
2021-09-13 18:46:41 +0100

    police

--- src/data/factions.ts
+++ src/data/factions.ts
@@ -55,7 +55,7 @@ export const npFactionsRegex = {
     police: noLater(noFormer(
         mergeRegex([
             /(?<!\bthen\b.*|!|\bformer\b[\s\w]+|corrections\s+|\bdoc\s+|\bmedic\w*\s+|\bems\s+)/i,
-            /(?:\bcop\b|chief of police|officer|der?puty|\bd-\d|\bdispatch\b|ride[\s\-_.]*along|sergeant|lieutenant|corporal|sheriff|trooper|cadet|\b(?:ranger|dt|sgt|cpl|ofc|lspd|sasp|bcso|cid|police[\s\-_]*academy|lt(?![^|!]*\bjones\b))\b)/i,
+            /(?:\bcop\b|chief of police|officer|der?puty|\bd-\d|\bdispatch\b|ride[\s\-_.]*along|sergeant|lieutenant|corporal|sheriff|trooper|cadet|\b(?:ranger|dt|sgt|cpl|ofc|lspd|sasp|bcso|police[\s\-_]*academy|lt(?![^|!]*\bjones\b))\b)/i,
             /(?!\?)/i,
         ])
     )),
