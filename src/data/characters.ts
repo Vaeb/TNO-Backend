5d7f818b03f566535fbdff2058c9ae3718a74e87
Adam Johnson <vaeb@hey.com>
2021-10-10 17:11:27 +0100

    add tommy

--- src/data/characters.ts
+++ src/data/characters.ts
@@ -1035,6 +1035,9 @@ export const npCharacters: NpCharacters = {
     HydraPlz: [
         { name: 'Gandalf "Stankyleg" Butters', factions: ['ASRR'] },
     ],
+    iamnxera: [
+        { name: '[Officer] Tommy Tinker', factions: ['Police'], displayName: 1, assumeServer: 'public' },
+    ],
     IAmSoul_RP: [
         { name: 'Johnathen Riker' },
     ],
