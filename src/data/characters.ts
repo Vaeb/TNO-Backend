07f9536bc1edccc86c3944e1a09598924f2675c5
Adam Johnson <vaeb@hey.com>
2021-10-12 20:50:01 +0100

    fix jaguar server

--- src/data/characters.ts
+++ src/data/characters.ts
@@ -1440,7 +1440,7 @@ export const npCharacters: NpCharacters = {
         { name: '[Sheriff] Kyle Pred', factions: ['Police'], leader: true, assumeServer: 'whitelist' },
         { name: '[Officer] Kyle Pred', factions: ['Police'], assumeServer: 'public' },
         { name: 'Dominic Toretti', displayName: 0, assumeServer: 'public' },
-        { name: 'Jaguar Toretti', displayName: 0, assumeServer: 'public' },
+        { name: 'Jaguar Toretti', displayName: 0 },
         { name: 'Alabaster Slim', nicknames: ['Pimp'], displayName: 2 },
         { name: 'Kegal Dan', factions: ['Dans'] },
         { name: 'Brett Biggledoinks' },
