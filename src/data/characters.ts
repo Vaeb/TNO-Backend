dfed18f39832c338944797bc373d526d4562840e
Adam Johnson <vaeb@hey.com>
2021-11-14 23:04:10 +0000

    fix klay lname

--- src/data/characters.ts
+++ src/data/characters.ts
@@ -340,7 +340,7 @@ export const npCharacters: NpCharacters = {
     ],
     Blaustoise: [
         { name: 'Mickey S', factions: ['Cleanbois', 'Clean Cartel'], nicknames: ['Downbad', 'Downrat'], assumeServer: 'whitelist' },
-        { name: 'Klay ?' },
+        { name: 'Klay Kona' },
         { name: 'Michael S.', factions: ['Gulag Gang'], nicknames: ['Upgood'], assumeServer: 'public' },
     ],
     bldrs: [
