525cce7dfc5c7d6fc825bdd7fe93578048176397
Adam Johnson <vaeb@hey.com>
2021-11-20 13:39:26 +0000

    Make D and H primary cb color

--- src/data/characters.ts
+++ src/data/characters.ts
@@ -1021,7 +1021,7 @@ export const npCharacters: NpCharacters = {
         { name: 'Blaine Burke', factions: ['Angels'] },
     ],
     Harry: [
-        { name: 'Harry Brown', factions: ['Limelight', 'Cleanbois', 'Pegasus', 'Rooster'] },
+        { name: 'Harry Brown', factions: ['Cleanbois', 'Limelight', 'Pegasus', 'Rooster'] },
     ],
     HasanAbi: [
         { name: 'Humberto Antonio Donato Pecorino', factions: ['Cleanbois', 'Rooster'], nicknames: ['Don', 'Donnie', reg(/\bOva[h']? here\b/)], displayName: 5 },
@@ -1293,7 +1293,7 @@ export const npCharacters: NpCharacters = {
         { name: 'Billy Phresh', factions: ['Street Team', 'Chang Gang'], displayName: 0 },
     ],
     Jonthebroski: [
-        { name: 'Denzel Williams', factions: ['Limelight', 'Cleanbois', 'Pegasus', 'Rooster'], nicknames: ['The "Cleaner"', 'Presidente'] },
+        { name: 'Denzel Williams', factions: ['Cleanbois', 'Limelight', 'Pegasus', 'Rooster'], nicknames: ['The "Cleaner"', 'Presidente'] },
         { name: '[Officer] Johnny Divine', factions: ['Police'], displayName: 0 },
         { name: 'Broskingle Dan', factions: ['Dans'] },
         { name: '[Senator] Sean Davis', factions: ['DoJ'], leader: true, nicknames: ['Senate'] },
