4c962b6fc29c61316527b08a54d1b9a08149df52
Adam Johnson <vaeb@hey.com>
2021-12-05 00:11:05 +0000

    Fix turk

--- src/data/characters.ts
+++ src/data/characters.ts
@@ -202,8 +202,8 @@ export const npCharacters: NpCharacters = {
         { name: '[Officer] Gio Coppola', factions: ['Police'], nicknames: ['466'], displayName: 1, assumeServer: 'public' },
     ],
     appyturk: [
+        { name: 'Appie Turk', factions: ['Gulag Gang'], displayName: 2, assumeServer: 'public' },
         { name: 'Appie Turk', factions: ['Gulag Gang'], displayName: 2, assumeServer: 'whitelist' },
-        { name: 'Appie Turk', factions: ['SSB'], displayName: 2, assumeServer: 'public' },
         { name: 'Appie Turk', displayName: 2, assumeServer: 'international' },
     ],
     Arcadum: [
