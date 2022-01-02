29fb56c4f2d9877b1ea2af2ea64a4597a25ce739
Adam Johnson <vaeb@hey.com>
2022-01-02 15:02:52 +0000

    Add medical callsign regex

--- src/data/factions.ts
+++ src/data/factions.ts
@@ -68,7 +68,7 @@ export const npFactionsRegex = {
             /(?!\?|[\s\-_]+copium)/i,
         ])
     )),
-    medical: noLater(/(?<!then\b.*|!)(?:doctor|paramedic|therapist|psychologist|\b(?:dr|em[st])\b)/i),
+    medical: noLater(/(?<!then\b.*|!)(?:doctor|\b[teplch]-\d{1,2}\b|paramedic|therapist|psychologist|\b(?:dr|em[st])\b)/i),
 } as { [key in NpFactionsRegexKeys]: RegExp };
 
 export const npFactionsSubRegex: { [key in FactionRealMini]?: [string, RegExp][] } = {
