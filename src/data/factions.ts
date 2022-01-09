733614cba65410236a651c7ae398c00b73d7acab
Adam Johnson <vaeb@hey.com>
2022-01-09 00:13:09 +0000

    Add to DoJ regex

--- src/data/factions.ts
+++ src/data/factions.ts
@@ -36,7 +36,7 @@ export const npFactionsRegex = {
     marabunta: noLater(noFormer(/\bmarabunta/i)),
     hoa: noFormer(/\bh[.\s]*o[.\s\-_]*[ab]\b|hogs\s*of\s*anarchy|home[\s\-]*owners[\s\-]*association/i),
     pinkgang: noFormer(/\bpittman|(?:pink|\bpit+?)[\s\-_.]*(?:gang|crew)/i), // [^\w\s(]\s*pc\s*[^\w\s)]
-    doj: noLater(noFormer(/\bdoj\b|department\s*of\s*justice|judge\b(?![\s]*me)|\bd\.a\b|\ba\.?d\.?a\b|(?<!\b(?:former|ex|aspiring)[\s\-]*)(?:lawyer|attorney)|para[\s\-]*legal/i)),
+    doj: noLater(noFormer(/\bdoj\b|\bspu\b|department\s*of\s*justice|judge\b(?![\s]*me)|\bd\.a\b|\ba\.?d\.?a\b|(?<!\b(?:former|ex|aspiring)[\s\-]*)(?:lawyer|attorney)|para[\s\-]*legal/i)),
     asrr: noLater(noFormer(/\balta[\s\-]*street|(?<!\bthe\s)\bblock(?:\b|[\s\-]*party|[\s\-]*athon)/i)),
     dans: noLater(noFormer(/\bdans\b|g[le][el][\s\-_.]+dan/i)),
     angels: noFormer(/(?<!rejected[\s\-_.]*)\bangels\b/i),
