0d0c0749f74033fdc21615e9ad9825a1f25b7218
Adam Johnson <vaeb@hey.com>
2022-01-21 20:36:49 +0000

    Fix displayname

--- src/data/characters.ts
+++ src/data/characters.ts
@@ -3280,7 +3280,7 @@ export const npCharacters: NpCharacters = {
     travpiper: [
         { name: 'Carlos "Cheddar" Sanchez', factions: ['HOA'] },
         { name: '[Deputy] William Gunner', factions: ['Police'], nicknames: ['Willy'] },
-        { name: '[Deputy] Jason Hannah', factions: ['Police'], displayName: 0 },
+        { name: '[Deputy] Jason Hannah', factions: ['Police'], displayName: 1 },
         { name: 'Warren Wallace' },
         { name: 'Charlie Colcord' },
         { name: 'Hazard' },
