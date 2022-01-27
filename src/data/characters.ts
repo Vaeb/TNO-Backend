2637afad5a2034c989a47bb5cbf22210cdfc5965
Adam Johnson <vaeb@hey.com>
2022-01-27 17:31:52 +0000

    Fix craig

--- src/data/characters.ts
+++ src/data/characters.ts
@@ -2904,7 +2904,7 @@ export const npCharacters: NpCharacters = {
         { name: 'Conrad Gross', assume: 'assumeOther' },
     ],
     SmithyFPS: [
-        { name: 'Craig ?', factions: ['SSB'], nicknames: ['Crackhead Craig'], displayName: 3, assume: 'assumeNpNoOther' },
+        { name: 'Craig Munson', nicknames: ['Crackhead Craig'], displayName: 3, assume: 'assumeNpNoOther' },
     ],
     SmithyPlaysUK: [
         { name: 'Reginald Parker', factions: ['DoC'] },
