8719733691760c17e4f1536dc9c76b7f7cfef359
Adam Johnson <vaeb@hey.com>
2022-02-04 23:47:00 +0000

    fix elo

--- src/data/characters.ts
+++ src/data/characters.ts
@@ -960,7 +960,7 @@ export const npCharacters: NpCharacters = {
     ],
     Elochai: [
         { name: 'Tao Chen', factions: ['Rooster'], assumeServer: 'whitelist' },
-        { name: '[Development] Elochai', factions: ['Development'], nicknames: ['working on', reg(/\b(?:cloth\w+|shirt|outfit|pants|helmet|hat)s?\b/)] },
+        { name: '[Development] Elochai', factions: ['Development'], nicknames: ['finishing', 'working on', reg(/\b(?:cloth\w+|shirt|outfit|pants|helmet|hat)s?\b/)] },
         { name: 'Leyin Jhail', assumeServer: 'public' },
         { name: 'Damien Grey', factions: ['Condemned MC'], displayName: 0 },
     ],
