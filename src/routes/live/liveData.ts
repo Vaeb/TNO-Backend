c39cfc1777acaece2c02d15f3de6f8416d783fb5
Adam Johnson <vaeb@hey.com>
2022-02-09 22:11:08 +0000

    Limit maxlookup

--- src/routes/live/liveData.ts
+++ src/routes/live/liveData.ts
@@ -963,7 +963,7 @@ export const getNpLive = async (baseOptions = {}, override = false, integrated =
                     baseHtml,
                     baseHtmlFb,
                     fbDebounce: 1000 * 60 * 5.5,
-                    fbMaxLookup: -1,
+                    fbMaxLookup: 3,
                     fbSleep: 2100,
                     fbGroupSize: 3,
                     fbGroupSleepInc: 1400,
