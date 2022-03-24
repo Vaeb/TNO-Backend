72db11ad2b869395b7ff8dfabb6c00516deac26f
Adam Johnson <vaeb@hey.com>
2022-03-24 20:27:25 +0000

    Add buddha

--- src/data/characters.ts
+++ src/data/characters.ts
@@ -511,6 +511,7 @@ export const npCharacters: NpCharacters = {
     ],
     buddha: [
         { name: 'Lang Buddha', factions: ['Cleanbois', 'Clean Cartel', 'Rooster'], leader: true, nicknames: ['Circle Andy', 'Timelord'], assumeChar: true, assumeServer: 'whitelist' },
+        { name: '[Officer] LeBron James', factions: ['Police'], nicknames: ['623'] },
         { name: '[Ranger] Lang Buddha', factions: ['Police'], displayName: 1, assumeServer: 'public' },
         { name: 'Esteban Julio-Ricardo-Montoya-De-La-Rosa Ramirez', factions: ['Police'] },
         { name: '[Deputy] Kevin Kona', factions: ['Police'] },
