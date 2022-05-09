a2659e2661887b8af7e5dceb85d5abc2058ee6d8
Adam Johnson <vaeb@hey.com>
2022-05-09 15:03:15 +0100

    Update rust regex

--- src/data/factions.ts
+++ src/data/factions.ts
@@ -45,7 +45,7 @@ export const npFactionsRegex = {
     mandem: noFormer(/\bmandem|\bmdma?\b(?![\s\-]+record)/i),
     roadmen: noFormer(/\broadmen/i),
     bbmc: noFormer(/\bbbmc\b|\bbondi/i),
-    rust: noLater(noFormer(/\br[\s\-_.]+u[\s\-_.]+s[\s\-_.]+t\b|\bunfortunate[\s\-_.]*street[\s\-_.]*team\b/i)),
+    rust: noLater(noFormer(/\br[\s\-_.]*u[\s\-_.]*s[\s\-_.]*t\b|\bunfortunate[\s\-_.]*street[\s\-_.]*team\b/i)),
     cbpd: noFormer(/c[\s\-_.]*b[\s\-_.]*p[\s\-_.]*d|(?:cerberus|\bares)[\s\-_.]*(?:business[\s\-_.]*)?(?:protection|security)|\btartarus/),
     italianmafia: noFormer(/\b(?:italian|sicilian)[\s\-_.]*(?:mafi|mob)|\bcosa[\s\-_.]*nostra|\bbalbani\b|\bbcf\b/i),
     royalmafia: noFormer(/zoo[\s\-_.]*mafi|\bzm\b/i),
