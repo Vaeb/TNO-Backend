bbef7c05f39ab6ed09b543c4f7beb83ac2c2618e
Adam Johnson <vaeb@hey.com>
2022-05-26 17:40:43 +0100

    Remove vinny pub

--- src/data/characters.ts
+++ src/data/characters.ts
@@ -3031,7 +3031,7 @@ export const npCharacters: NpCharacters = {
     ],
     Shotz: [
         { name: 'Vinny Pistone', factions: ['Chang Gang', 'Chaos', 'Tuner Shop'], assumeServer: 'whitelist' },
-        { name: 'Vinny Pistone', factions: ['Gulag Gang'], assumeServer: 'public' },
+        // { name: 'Vinny Pistone', factions: ['Gulag Gang'], assumeServer: 'public' },
         { name: '[Ranger] John Mineo', factions: ['Police'], nicknames: ['Corporal'] },
         { name: 'Mr J', factions: ['Chang Gang'], displayName: 0 },
     ],
