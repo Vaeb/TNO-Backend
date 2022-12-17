82f59b2c71b266816aef4d1b48da723a44e14938
Adam Johnson <vaeb@hey.com>
2022-12-17 16:54:40 +0000

    Fix blown

--- src/data/characters.ts
+++ src/data/characters.ts
@@ -485,7 +485,7 @@ export const npCharacters: NpCharacters = {
         { name: 'Dillon Dubbs', factions: ['News'], displayName: 0 },
     ],
     Blown: [
-        { name: 'Taylor "Spaceman" Briggs', factions: ['Seaside'] },
+        { name: 'Taylor "Spaceman" Briggs' },
     ],
     bLuE622: [
         { name: 'Boe Jangles', factions: ['Chang Gang'], affiliate: true },
